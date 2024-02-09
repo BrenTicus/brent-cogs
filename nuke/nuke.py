147323f3cb98ab598bf2bfcbe0d97512a35d3e75
Brent Glowinski <brent.glowinski@pm.me>
2024-02-09 10:20:31 -0700

    Cleanup, probably including bug fixes.

--- nuke/nuke.py
+++ nuke/nuke.py
@@ -1,8 +1,7 @@
 import asyncio
-import discord
-import pathlib
-from redbot.core import checks, Config, commands, app_commands
-from redbot.core.data_manager import cog_data_path
+from typing import List
+from redbot.core import checks, Config, commands
+from discord import Guild, Member
 
 
 class Nuke(commands.Cog):
@@ -15,7 +14,7 @@ class Nuke(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=235841895)
 
-    async def _remove_users(*users):
+    async def _remove_users(self, users: List[Member]):
         """Actually kick the users from the guild."""
         waitlist = []
         for user in users:
@@ -23,7 +22,6 @@ class Nuke(commands.Cog):
 
         await asyncio.wait(waitlist, return_when=asyncio.ALL_COMPLETED)
 
-    @commands.command("nuke")
     @commands.guild_only()
     @checks.admin_or_permissions(manage_guild=True)
     async def nuke(self, ctx: commands.Context, *names):
@@ -36,9 +34,12 @@ class Nuke(commands.Cog):
     @checks.admin_or_permissions(manage_guild=True)
     async def nuke_not(self, ctx: commands.Context, *names):
         """Removes users from the guild except those matching the group names."""
-        all_roles = await fetch_roles()
-        guild = ctx.guild
+        if ctx.guild is None:
+            return
+        guild: Guild = ctx.guild
+        all_roles = await guild.fetch_roles()
         exclude_people = []
+        misses = []
         # Go over the provided roles and
         for name in names:
             role_filter = [x for x in all_roles if x.name == name]
@@ -50,9 +51,9 @@ class Nuke(commands.Cog):
         if misses:
             miss_text = ", ".join(misses)
             await ctx.channel.send(
-                f"The following roles could not be found: {misses}.\nNot running until all roles are recognized."
+                f"The following roles could not be found: {miss_text}.\nNot running until all roles are recognized."
             )
             return
-        all_members = ctx.guild.members
+        all_members = guild.members
         kickable_members = [x for x in all_members if x not in exclude_people]
-        await _remove_users(kickable_members)
+        await self._remove_users(kickable_members)
